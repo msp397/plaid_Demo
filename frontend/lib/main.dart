fa10289ea9760a6a4bd004ba26417df60f2f12da
rahulsakthi306 <rahuls@torus.tech>
2024-08-13 18:55:07 +0530

    latest changes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:frontend/account_info.dart';
 import 'package:frontend/utils/urls.dart';
 import 'package:http/http.dart' as http;
 import 'package:plaid_flutter/plaid_flutter.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'dart:html' as html;
-import 'dart:js_util' as js_util;
+// import 'dart:html' as html;
+// import 'dart:js_util' as js_util;
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bank Transfer App',
+      title: 'TPay',
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
@@ -35,6 +36,13 @@ class _HomeScreenState extends State<HomeScreen> {
   LinkConfiguration? _configuration;
   String linkToken = '';
 
+  @override
+  void initState() {
+    super.initState();
+    _createLinkToken();
+    _setupPlaidLinkStreams();
+  }
+
   Future<void> _createLinkToken() async {
     try {
       final response = await http.post(
@@ -44,13 +52,12 @@ class _HomeScreenState extends State<HomeScreen> {
           "user": {"client_user_id": "App123"},
           "client_name": "Plaid App",
           "products": ["auth"],
-          "country_codes": ["GB"],
+          "country_codes": ["US"],
           "language": "en",
         }),
       );
       print(response.body);
       print(response.statusCode);
-
       if (response.statusCode == 200) {
         linkToken = jsonDecode(response.body)['link_token'];
         print(linkToken);
@@ -70,37 +77,69 @@ class _HomeScreenState extends State<HomeScreen> {
       _openPlaidLinkWeb();
     } else {
       if (_configuration != null) {
-        PlaidLink.open(configuration: _configuration!);
+        PlaidLink.open(
+          configuration: _configuration!,
+        );
       }
     }
   }
 
+  void _setupPlaidLinkStreams() {
+    PlaidLink.onSuccess.listen((success) {
+      print('Account Added SuccessFully');
+      print(success.publicToken);
+      print(success.metadata.accounts);
+      print(success.metadata.institution);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => AccountInfo()),
+      );
+    });
+
+    PlaidLink.onExit.listen((exit) {
+      if (exit.error != null) {
+        print('Plaid Link Exit: ${exit.error}');
+      } else {
+        print('Plaid Link exited without error');
+      }
+    });
+  }
+
   void _openPlaidLinkWeb() {
-    if (_configuration != null) {
-      js_util.callMethod(html.window, 'openPlaidLink', [linkToken]);
-    }
+    // if (_configuration != null) {
+    //   js_util.callMethod(html.window, 'openPlaidLink', [linkToken]);
+    // }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Bank Transfer App')),
+      appBar: AppBar(
+        title: Text('Bank transfer via TPay'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: _createLinkToken,
-              child: Text("Create Link Token"),
+            Icon(
+              Icons.account_balance,
+              size: 70,
+              color: Colors.blue,
             ),
             SizedBox(height: 16),
             ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.blue),
+                foregroundColor: WidgetStatePropertyAll(Colors.white),
+              ),
               onPressed: _configuration != null
                   ? kIsWeb
                       ? _openPlaidLinkWeb
                       : _handleAddBank
                   : null,
-              child: Text('Add Bank'),
+              child: Text('Add Bank Account'),
             ),
           ],
         ),
